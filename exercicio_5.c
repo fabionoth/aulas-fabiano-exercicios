6ba6bb770a72cb1644d9885214c6a832023046fa
Fabio Noth <fabio.noth@gmail.com>
2017-03-15 18:34:08 -0300

    inclusão exercício 5

---
+++ exercicio_5.c
@@ -0,0 +1,24 @@
+/*
+Um inteiro positivo N é perfeito se for igual a soma de seus divisores positivos diferentes de N
+
+Exemplo: 6 é perfeito pois 1+2+3 = 6 e 1, 2, 3 são todos os divisores positivos de 6 e que são diferentes de 6.
+Faça um programa em C que recebe como entrada um número positivo K e mostre os K primeiros números perfeitos.
+
+http://www.matematicadidatica.com.br/Divisores.aspx
+
+*/
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+
+int i = 60;
+
+int main()
+{
+    
+}
