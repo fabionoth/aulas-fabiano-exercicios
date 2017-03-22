8cd6bc380c50a368690415d57585f15d0ae106c0
fabionoth <fabio.noth@gmail.com>
2017-03-21 21:37:27 -0300

    exercicios_inicio

---
+++ exercicio_sala/pilha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Pilha{
+     int topo;
+     int valor;   
+} pilha;
+
+void inicia(){
+    
+}
---
+++ exercicio_sala_aula2/pintor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//#include <assert.h>
+//#include <limits.h>
+//#include <stdbool.h>
+
+int tam;
+int[][] m;
+
+void gera_matriz(int tam);
+void pinta(int [][]m, int i, int j, int cor, int fundo);
+int main() {
+
+}
+
+void gera_matriz(){
+    
+}
+
+void pinta(int [][]m, int i, int j, int cor, int fundo){
+
+}
